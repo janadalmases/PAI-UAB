8b6e6c1ee8b53997816acb7b8202480e1b896558
janadalmases <uabjana0@gmail.com>
2024-12-19 15:01:17 +0100

    .

--- projecte_final/de_nou/primera_part_copy.js
+++ projecte_final/de_nou/primera_part_copy.js
@@ -1,25 +1,22 @@
 let errors = false
 
 // Nom i cognoms
-function noms(){  
-  const nom = document.getElementById("nom_cognoms");
-  let nomValors = nom.value.toLowerCase().trim().split(/\s+/); // Divideix el text en paraules
-  let majusculesCorrectes = true;
-
-  for (let paraula of nomValors) {
-    if (paraula[0] !== paraula[0].toUpperCase()) {
-      // Si la primera lletra no és majúscula, marca l'error
-      majusculesCorrectes = false;
-      break;
-    }
+let majusculesCorrectes = true
+const nom = document.getElementById("nom_cognoms");
+const nomValors = nom.value.toLowerCase().split(" "); // Divideix el text en paraules
+for (let paraula of nomValors) {
+  if (paraula[0] !== paraula[0].toUpperCase()) {
+    // Si la primera lletra no és majúscula, marca l'error
+    majusculesCorrectes = false;
+    break;
   }
+}
 
-  if (!majusculesCorrectes) {
-    document.getElementById("error-nom").textContent = "Escriviu en majúscules la primera lletra de cada paraula.";
-    errors = true;
-  } else {
-    document.getElementById("error-nom").textContent = "";
-  }
+if (!majusculesCorrectes) {
+  document.getElementById("error-nom").textContent = "Escriviu en majúscules la primera lletra de cada paraula.";
+  errors = true;
+} else {
+  document.getElementById("error-nom").textContent = "";
 }
 
 /*for (let i = 0; i < paraules.length; i++) {
@@ -32,7 +29,7 @@ nom.value= paraules.join(" ");*/
 
 
 // Resultat
-if (errors==) {
+if (!errors) {
   document.getElementById("resultat").textContent = "Formulari enviat correctament!";
 } else {
   document.getElementById("resultat").textContent = "";
